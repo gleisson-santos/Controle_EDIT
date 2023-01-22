8a7818a1cf9bbb64aa3d9a8f74ce6ee12b186fa7
Gleisson Santos <jsgleisson@gmail.com>
2023-01-22 01:08:41 -0300

    commit

---
+++ controle_pav/static/js/scripts.js
@@ -0,0 +1,19 @@
+const btn = document.querySelector("#show"); /* Buttton */
+const container2 = document.querySelector(".alert-success");
+
+btn.addEventListener("click", function() {
+    
+    if(container2.style.display === 'block') {
+        container2.style.display = 'none';
+    } else {
+        container2.style.display = 'block';
+    }
+    
+    setTimeout(fechar,  4000) 
+    
+    // console.log('Fui clicado!!');
+});
+
+function fechar () {
+    container2.style.display = 'none';
+}
