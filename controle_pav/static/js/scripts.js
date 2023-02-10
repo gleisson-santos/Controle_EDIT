947d5fc540edfef0d83f1a6cf4230bb1e1b1ad76
Glfx20 <jsgleisson@gmail.com>
2023-02-10 15:42:48 -0300

    commit

--- controle_pav/static/js/scripts.js
+++ controle_pav/static/js/scripts.js
@@ -1,21 +1,21 @@
-const btn = document.querySelector("#show"); /* Buttton */
-const container2 = document.querySelector(".alert-success");
+// const btn = document.querySelector("#show"); /* Buttton */
+// const container2 = document.querySelector(".alert-success");
 
-btn.addEventListener("click", function() {
+// btn.addEventListener("click", function() {
     
-    if(container2.style.display === 'block') {
-        container2.style.display = 'none';
-    } else {
-        container2.style.display = 'block';
-    }
+//     if(container2.style.display === 'block') {
+//         container2.style.display = 'none';
+//     } else {
+//         container2.style.display = 'block';
+//     }
     
-    setTimeout(fechar,  4000) 
+//     setTimeout(fechar,  4000) 
     
-    // console.log('Fui clicado!!');
-});
+//     // console.log('Fui clicado!!');
+// });
 
-function fechar () {
-    container2.style.display = 'none';
-}
+// function fechar () {
+//     container2.style.display = 'none';
+// }
 
 
