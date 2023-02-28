5c25a81fbd45d9d030cdfd7c1d053bbd54a74605
Glfx20 <jsgleisson@gmail.com>
2023-02-28 12:20:49 -0300

    Update tests.py

--- controle_pav/tests.py
+++ controle_pav/tests.py
@@ -1,22 +1,28 @@
-from email import contentmanager
-from ssl import HAS_TLSv1_1
-from django.test import TestCase
-
-# Create your tests here.
-from django.test import TestCase, RequestFactory
-from controle_pav.views import index
-
+from django.contrib.auth import get_user_model, authenticate, login, logout, SESSION_KEY, get_user_model
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.decorators import login_required, permission_required, user_passes_test
 from django.test import TestCase, RequestFactory
 from django.urls import reverse
-from myapp.views import index
+from .models import *
+from .views import *
+from datetime import date, datetime, time, timedelta
+from django.utils import timezone
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.models import User
+from django.contrib.auth import force_login 
 
 class IndexViewTestCase(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
+        self.user = User.objects.create_user(
+            username='testuser', email='testuser@test.com', password='testpassword'
+        )
 
     def test_index_view(self):
         url = reverse('index')
         request = self.factory.get(url)
+        request.user = self.user
         response = index(request)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'dados/index.html')
